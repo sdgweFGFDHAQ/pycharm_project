5b96dfa2654ab8bad2f089189b104b3993f9a973
“Your <179682664@qq.com>
2022-12-13 17:40:26 +0800

    updata

--- workplace/forone/get_standard_data.py
+++ workplace/forone/get_standard_data.py
@@ -30,6 +30,8 @@ def cut_word(word):
     stop_words = [line.strip() for line in open('../stop_word_plug.txt', 'r', encoding='utf-8').readlines()]
     word = re.sub(r'\(.*?\)', '', word)
     word = re.sub(r'[^a-zA-Z0-9\u4e00-\u9fa5]|[丨]', '', word)
+    # 形如:"EXO店x铺excelAxB" 去除x
+    word = re.sub(r'(?<=[\u4e00-\u9fa5])(x|X)(?=[\u4e00-\u9fa5])|(?<=[A-B])x(?=[A-B])', '', word)
     # 不可分割的词
     with open('../inseparable_word_list.txt', 'r', encoding='utf-8') as in_word:
         for iw in in_word:
