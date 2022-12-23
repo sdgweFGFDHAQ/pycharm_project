4d53d635e3f8485bc36233f0d5bc8b4e5cc87298
周兆雄 <1796822664@qq.com>
2022-12-23 16:43:03 +0800

    update

--- testcode/test_vectorization.py
+++ testcode/test_vectorization.py
@@ -2,22 +2,22 @@ from ast import literal_eval
 
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfTransformer, TfidfVectorizer
-from sklearn.naive_bayes import ComplementNB
+from sklearn.naive_bayes import ComplementNB,BernoulliNB, MultinomialNB
+from workplace.forone.count_category_num import count_the_number_of_categories
 
 
 def test_transform():
     result = pd.DataFrame()
     csv_data = pd.read_csv('./aaa.csv', usecols=['name', 'category3_new', 'cut_name'])
     csv_data['cut_name'] = csv_data['cut_name'].apply(literal_eval)
-    x = csv_data['name']
+    x = count_the_number_of_categories(csv_data)
     y = csv_data['category3_new']
     c_nb = ComplementNB()
-    transfer = TfidfVectorizer()
-    X = transfer.fit_transform(list(x))
-    print(X)
-    c_nb.fit(X, y)
-    print(c_nb.predict(X))
-    print("准确率为:", c_nb.score(X, y))
+    # transfer = TfidfTransformer()
+    # x = transfer.fit_transform(x)
+    print("拟合：：", x)
+    c_nb.fit(x, y)
+    print(c_nb.feature_log_prob_)
 
 
 if __name__ == '__main__':
