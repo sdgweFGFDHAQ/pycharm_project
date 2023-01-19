bcf36e2690a24914e16b3ab6d30cb04523244dfa
“Your <179682664@qq.com>
2023-01-19 17:08:37 +0800

    update

--- workplace/hivesql/single_source_data_judge.py
+++ workplace/hivesql/single_source_data_judge.py
@@ -1,8 +1,4 @@
-from pyhive import hive
-from impala.dbapi import connect
-from impala.util import as_pandas
 import pandas as pd
-import numpy as np
 import time
 import datetime
 from dateutil.relativedelta import relativedelta
@@ -10,7 +6,19 @@ from dateutil.relativedelta import relativedelta
 
 # 2对df判断半年内数据。根据数据分布设置（陈列活动、进货交易、拜访记录）次数阈值critical_weight
 def get_threshold(x_df: pd.DataFrame, time_slot: int):
-    c_6_month_df1 = x_df[x_df.create_time.apply(int) > time_slot]
+    c_6_month_df1 = x_df[x_df.createtime.apply(int) > time_slot]
+    group_count1 = c_6_month_df1['storeid'].value_counts()
+    data_frame1 = pd.DataFrame({'storeid': group_count1.index, 'count': group_count1}).reset_index()
+    print(data_frame1)
+    critical_weight = data_frame1['count'].median()
+    print("次数阈值（中位数）:", critical_weight)
+    return critical_weight
+
+
+# 数据为8位时间字符串
+def get_threshold_8(x_df: pd.DataFrame, time_slot):
+    ts = str(time_slot).replace('-', '')
+    c_6_month_df1 = x_df[x_df.createtime.apply(str) > ts]
     group_count1 = c_6_month_df1['storeid'].value_counts()
     data_frame1 = pd.DataFrame({'storeid': group_count1.index, 'count': group_count1}).reset_index()
     print(data_frame1)
@@ -31,7 +39,7 @@ def get_weight(x_df: pd.DataFrame, today_date):
 
 # 数据为8位时间字符串
 def get_weight_8(x_df: pd.DataFrame, today_date):
-    x_df['createtime'] = pd.to_datetime(x_df['createtime'], format='%Y%m%d')
+    x_df['createtime'] = pd.to_datetime(x_df['createtime'], format='%Y%m%d', errors='coerce')
     x_df['month'] = x_df['createtime'].apply(
         lambda x: (today_date - x).days / 30)
     x_df['weight'] = x_df['month'].apply(lambda x: 1 / 1 + 0.8 * x if x > 6 else 1.0)
@@ -42,15 +50,16 @@ def get_weight_8(x_df: pd.DataFrame, today_date):
 
 def judge_data(x_df1: pd.DataFrame, x_df2: pd.DataFrame, x_df3: pd.DataFrame):
     today_date = datetime.date.today()
+    today_datetime = datetime.datetime.now()
     font_6_time = int(time.mktime(time.strptime(str(today_date - relativedelta(months=6)), "%Y-%m-%d")))
     critical_weight1 = get_threshold(x_df1, font_6_time)
     critical_weight2 = get_threshold(x_df2, font_6_time)
-    critical_weight3 = get_threshold(x_df3, font_6_time)
+    critical_weight3 = get_threshold_8(x_df3, today_date - relativedelta(months=6))
     critical_weight = (critical_weight1 + critical_weight2 + critical_weight3) / 3
     # 3根据时间-权重映射函数计算单源企业数据的权重值weight
     sum_weight1 = get_weight(x_df1, today_date)
     sum_weight2 = get_weight(x_df2, today_date)
-    sum_weight3 = get_weight_8(x_df3, today_date)
+    sum_weight3 = get_weight_8(x_df3, today_datetime)
     # 对陈列、交易和拜访表赋权重 a=0.2 b=0.5 c=0.3
     sum_weight1['weight'] = sum_weight1['weight'].apply(lambda x: 0.2 * x)
     sum_weight2['weight'] = sum_weight2['weight'].apply(lambda x: 0.5 * x)
@@ -70,6 +79,6 @@ def judge_data(x_df1: pd.DataFrame, x_df2: pd.DataFrame, x_df3: pd.DataFrame):
 if __name__ == '__main__':
     display_df = pd.read_csv('display_data.csv', usecols=['storeid', 'createtime'])
     order_df = pd.read_csv('order_data.csv', usecols=['storeid', 'createtime'])
-    visit_df = pd.read_csv('visit_data.csv', usecols=['storeid', 'createtime'])
+    visit_df = pd.read_csv('visit_data.csv', usecols=['storeid', 'createtime'],
+                           dtype={"storeid": str, "createtime": str})
     judge_data(display_df, order_df, visit_df)
-
