eeca725a22a4536c129899cc8398b3c4f07cf166
zhouzhaoxiong <zhouzhaoxiong@wxchina.com>
2023-08-16 14:15:25 +0800

    update

--- workbranch/ifexist_hivesql/single_source_data_judge.py
+++ workbranch/ifexist_hivesql/single_source_data_judge.py
@@ -8,8 +8,8 @@ import math
 # 2对df判断半年内数据。根据数据分布设置（陈列活动、进货交易、拜访记录）次数阈值critical_weight
 def get_threshold(x_df: pd.DataFrame, time_slot: int):
     c_6_month_df1 = x_df[x_df.createtime.apply(int) > time_slot]
-    group_count1 = c_6_month_df1['storeid'].value_counts()
-    data_frame1 = pd.DataFrame({'storeid': group_count1.index, 'count': group_count1}).reset_index()
+    group_count1 = c_6_month_df1['id'].value_counts()
+    data_frame1 = pd.DataFrame({'id': group_count1.index, 'count': group_count1}).reset_index()
     print(data_frame1)
     critical_weight = data_frame1['count'].median()
     print("次数阈值（中位数）:", critical_weight)
@@ -20,8 +20,8 @@ def get_threshold(x_df: pd.DataFrame, time_slot: int):
 def get_threshold_8(x_df: pd.DataFrame, time_slot):
     ts = str(time_slot).replace('-', '')
     c_6_month_df1 = x_df[x_df.createtime.apply(str) > ts]
-    group_count1 = c_6_month_df1['storeid'].value_counts()
-    data_frame1 = pd.DataFrame({'storeid': group_count1.index, 'count': group_count1}).reset_index()
+    group_count1 = c_6_month_df1['id'].value_counts()
+    data_frame1 = pd.DataFrame({'id': group_count1.index, 'count': group_count1}).reset_index()
     print(data_frame1)
     critical_weight = data_frame1['count'].median()
     print("次数阈值（中位数）:", critical_weight)
@@ -34,7 +34,7 @@ def get_weight(x_df: pd.DataFrame, today_date):
         lambda x: (today_date - datetime.date.fromtimestamp(int(x) // 1000)).days / 30)
     x_df['weight'] = x_df['month'].apply(lambda x: 0.6 ** (x - 6) if x > 6 else 1.0)
     print(x_df)
-    sum_weight = x_df[['storeid', 'weight']].groupby(by='storeid').sum()
+    sum_weight = x_df[['id', 'name', 'namepath', 'township', 'address', 'weight']].groupby(by=['id', 'name', 'namepath', 'township', 'address']).sum().reset_index()
     return sum_weight
 
 
@@ -45,7 +45,7 @@ def get_weight_8(x_df: pd.DataFrame, today_date):
         lambda x: (today_date - x).days / 30)
     x_df['weight'] = x_df['month'].apply(lambda x: 0.9 ** (x - 6) if x > 6 else 1.0)
     print(x_df)
-    sum_weight = x_df[['storeid', 'weight']].groupby(by='storeid').sum()
+    sum_weight = x_df[['id', 'name', 'namepath', 'township', 'address', 'weight']].groupby(by=['id', 'name', 'namepath', 'township', 'address']).sum().reset_index()
     return sum_weight
 
 
@@ -66,10 +66,12 @@ def judge_data(x_df1: pd.DataFrame, x_df2: pd.DataFrame, x_df3: pd.DataFrame):
     sum_weight1['weight'] = sum_weight1['weight'].apply(lambda x: 0.2 * x)
     sum_weight2['weight'] = sum_weight2['weight'].apply(lambda x: 0.5 * x)
     sum_weight3['weight'] = sum_weight3['weight'].apply(lambda x: 0.3 * x)
-    s_w = sum_weight1.add(sum_weight2, fill_value=0)
-    sum_weight = s_w.add(sum_weight3, fill_value=0)
+    # 合并数据框
+    merged_df = pd.concat([sum_weight1, sum_weight2, sum_weight3], ignore_index=True)
+    # 对 'weight' 列进行求和
+    sum_weight = merged_df.groupby(['id', 'name', 'namepath', 'township', 'address'])['weight'].sum().reset_index()
     # 4根据最终的sum_weight和critical_weight打标，字段为is_existence（是否存在），计算percentage（存在概率）
-    percent_df = pd.DataFrame(sum_weight).reset_index()
+    percent_df = sum_weight.copy()
     # ===设置阈值，降低最大值========
     # percent_df_max = percent_df[percent_df.weight > critical_weight]
     # print(percent_df_max.index.size)
@@ -84,8 +86,7 @@ def judge_data(x_df1: pd.DataFrame, x_df2: pd.DataFrame, x_df3: pd.DataFrame):
     # result_df = df_1_2_3[df_1_2_3.percentage >= critical_percentage]
     result_df = df_1_2.sort_values(by='percentage', ascending=False).reset_index()
     print(result_df)
-    result_df.to_csv('result_df.csv')
-
+    result_df.to_csv('result_df.csv', columns=['id', 'name', 'namepath', 'township', 'address', 'percentage'])
 
 # Y=a+k(X-Min)
 def separate_percent(df, a, b):
@@ -104,8 +105,10 @@ def separate_percent1(df):
 
 
 if __name__ == '__main__':
-    display_df = pd.read_csv('display_data.csv', usecols=['storeid', 'createtime'])
-    order_df = pd.read_csv('order_data.csv', usecols=['storeid', 'createtime'])
-    visit_df = pd.read_csv('visit_data.csv', usecols=['storeid', 'createtime'],
-                           dtype={"storeid": str, "createtime": str})
+    columns = ['id', 'name', 'namepath', 'township', 'address', 'createtime']
+    display_df = pd.read_csv('display_data.csv')
+    order_df = pd.read_csv('order_data.csv')
+    visit_df = pd.read_csv('visit_data.csv',dtype={"createtime": str})
+    print('--开始计算置信率--')
     judge_data(display_df, order_df, visit_df)
+    print('--置信率计算完成--')
